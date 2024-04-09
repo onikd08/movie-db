f4ea7aef6f21e0eb7338c2360488f5417f67b25e
onikd08 <onikd08@gmail.com>
2024-04-09 05:45:02 +0300

    Add header footer and sidebar components

---
+++ components/Header.jsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="container flex items-center justify-between space-x-10 py-6">
+        <Link href="/">
+          <Image src="/assets/logo.svg" width="139" height="26" alt="logo" />
+        </Link>
+
+        <ul className="flex items-center space-x-5">
+          <li>
+            <Link
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <Image src="/assets/ring.svg" width="24" height="24" alt="ring" />
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <Image
+                src="/assets/icons/sun.svg"
+                width="24"
+                height="24"
+                alt="sun"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <Image
+                src="/assets/shopping-cart.svg"
+                width="24"
+                height="24"
+                alt="cart"
+              />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/SideBar.jsx
@@ -0,0 +1,85 @@
+import { getDictionary } from "@/app/[lang]/dictionaries/dictionaries";
+import Image from "next/image";
+import Link from "next/link";
+
+const SideBar = async ({ lang }) => {
+  const dict = await getDictionary(lang);
+  return (
+    <aside>
+      <ul className="space-y-2">
+        <li>
+          <Link
+            className="flex items-center space-x-2 px-5 py-3.5 rounded-lg bg-primary text-black"
+            href="#"
+          >
+            <Image
+              src="/assets/icons/trending.svg"
+              width="24"
+              height="24"
+              alt={dict.trending}
+            />
+            <span>{dict.trending}</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+            href="#"
+          >
+            <Image
+              src="/assets/icons/newRelease.svg"
+              width="24"
+              height="24"
+              alt={dict.newReleases}
+            />
+            <span>{dict.newReleases}</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+            href="#"
+          >
+            <Image
+              src="/assets/icons/commingSoon.svg"
+              width="24"
+              height="24"
+              alt={dict.comingSoon}
+            />
+            <span>{dict.comingSoon}</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+            href="#"
+          >
+            <Image
+              src="./assets/icons/favourite.svg"
+              width="24"
+              height="24"
+              alt={dict.favourites}
+            />
+            <span>{dict.favourites}</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+            href="#"
+          >
+            <Image
+              src="/assets/icons/watchLater.svg"
+              width="24"
+              height="24"
+              alt={dict.watchLater}
+            />
+            <span>{dict.watchLater}</span>
+          </Link>
+        </li>
+      </ul>
+    </aside>
+  );
+};
+
+export default SideBar;
